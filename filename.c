eabde911832143e4037cdf11c7bb04fbd19d13c0
FortuneQvQ <2719811032@qq.com>
2025-10-20 21:47:06 +0800

    Add files via upload

---
+++ filename.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define cs 100
+int main(void)
+{
+	int lx,lxz, y; float q[5] = { cs,cs,cs,cs,cs };
+	printf("年利率、多少年:\n");
+	scanf_s(" %d %d" ,&lx, &y);
+	printf("年\t");
+	for (int i = 0; i <= 4; i++)
+	{
+		printf("  %d%%\t",lx+i);
+	}
+	printf("\n");
+	for (int n = 1; n <= y; n++)
+	{
+		printf("%d\t", n);
+		for (int j = 0; j <= 4; j++)
+		{
+			lxz = lx + j;
+			q[j] *= (1 + lxz * 0.01);
+			printf("%5.3f\t", q[j]);
+		}
+		printf("\n");
+	}
+
+}
