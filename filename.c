9c31de62f84706ebf24c4d8889dcf11c8dc6eb79
FortuneQvQ <2719811032@qq.com>
2025-10-23 16:17:13 +0800

    Add files via upload

---
+++ filename.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	int a[26] = { 0 };
+	char c;
+	scanf_s(" ");
+	for (;;)
+	{
+		c = toupper(getchar());
+		for (int i = 0; i <= 25; i++)
+		{
+			if (c == 'a' + i)
+				a[i] += 1;
+		}
+		if (c == '\n')
+			break;
+	}
+	scanf_s(" ");
+	for (;;)
+	{
+		c = toupper(getchar()0;
+		for (int i = 0; i <= 25; i++)
+		{
+			if (c == 'a' + i)
+				a[i] -= 1;
+		}
+		if (c == '\n')
+			break;
+	}
+	for (int i = 0; i <= 25; i++)
+	{
+		if (a[i] != 0)
+		{
+			printf("不是");
+			return 0;
+		}
+	}
+	printf("是的");
+}
