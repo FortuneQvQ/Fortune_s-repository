123a239590eebba242619f6c10f428366e19ad88
FortuneQvQ <2719811032@qq.com>
2025-10-23 17:01:03 +0800

    Add files via upload

---
+++ filename.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#define n 3
+
+int main(void)
+{
+	int a[n][n] = { 0 }, x = n / 2, y = 0;
+	for (int i = 1; i <= n * n;i++)
+	{
+		a[x][y] = i;
+		if (a[(x + 1) % n][(y + 1) % n] != 0)
+		{
+			y = (y - 1 + n) % n;
+		}
+		else
+		{
+			x = (x + 1) % n; y = (y + 1) % n;
+		}
+	}
+	for (int i = 0; i <= n - 1; i++)
+	{
+		for (int j = 0; j <= n - 1; j++)
+		{
+			printf("%d\t", a[j][i]);
+		}
+		printf("\n");
+	}
+}
